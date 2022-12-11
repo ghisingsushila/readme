3aea0e6bd1522af8cf45ae72c66f672587656849
ghisingsushila <119056048+ghisingsushila@users.noreply.github.com>
2022-12-11 17:45:09 +0545

    triangle

---
+++ triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class triangle {
+    public:
+    double base, height;
+    void areaTri(){
+        cout<<"Enter base of the triangle:";
+        cin>>base;
+        cout<<"Enter height of the triangle:";
+        cin>>height;
+        cout<<"The area of triangle is"<<(0.5*base*height)<<"cm2"; 
+    }
+};
+int main(){
+    triangle t1;
+    t1. areaTri();
+    return 0;
+}
